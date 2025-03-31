16a560b670163ff3be48bfbfdf854e2acc2ed945
Nelvin <nelvinq@gmail.com>
2025-03-31 21:27:55 +0800

    feat fix image

--- mycocktail/settings.py
+++ mycocktail/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mycocktail', 'static'),
+    os.path.join(BASE_DIR, 'main_app/static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
